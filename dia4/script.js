794e3ad5a813f971b777ff613f431edecf92f1e9
Icaro Joel <icaromoura.programador@gmail.com>
2023-07-14 15:19:20 -0300

    end of pixel art project

--- dia4/script.js
+++ dia4/script.js
@@ -1,9 +1,9 @@
 // DESAFIO BONUS - Crie um site de fazer Pixel art
 const board = document.querySelector('.board')
-// const pixels = document.querySelectorAll('.pixel')
 const colors = document.querySelectorAll('.color')
 const createBtn = document.querySelector('.create')
 const removeBtn = document.querySelector('.clean')
+const returnBtn = document.querySelector('.return')
 
 colors.forEach((color) => {
   color.addEventListener('click', ({ target }) => {
@@ -14,46 +14,73 @@ colors.forEach((color) => {
   })
 })
 
-// pixels.forEach((pixel) => {
-//   pixel.addEventListener('click', ({ target }) => {
-//     const divColor = document.querySelector('.selected')
-//     let color;
-
-//     if (divColor.value) {
-//       color = divColor.value
-//     } else {
-//       color = window.getComputedStyle(divColor).getPropertyValue('background-color')
-//     }
-
-//     target.style.backgroundColor = color
-//   })
-// })
-
-createBtn.addEventListener('click', () => {
-  const lines = document.querySelector('.c-line').value
-  const pixels = document.querySelector('.c-pixel').value
+createBtn.addEventListener('click', async () => {
+  const lines = document.querySelector('.c-line')
+  const pixels = document.querySelector('.c-pixel')
 
   board.innerHTML = ''
 
-  for(let index = 0; index < Number(lines); index += 1) {
+  localStorage.setItem('last-pixel', JSON.stringify([]))
+  localStorage.removeItem('board')
+
+  for(let index = 0; index < Number(lines.value); index += 1) {
     const div = document.createElement('div')
 
     div.className = 'line'
 
-    for(let indexY = 0; indexY < Number(pixels); indexY += 1) {
+    for(let indexY = 0; indexY < Number(pixels.value); indexY += 1) {
       const pixel = document.createElement('div')
 
       pixel.className = 'pixel'
+      pixel.id = `${index}-${indexY}`
 
-      pixel.addEventListener('click', ({ target }) => {
+      const pixelData = {
+        l: index,
+        p: indexY,
+        c: 'rgb(255, 255, 255)',
+      }
+      
+      const crud = localStorage.getItem('board')
+      
+      if (crud) {
+        const data = await JSON.parse(crud)
+        localStorage.setItem('board', JSON.stringify([...data, pixelData]))
+      } else {
+        localStorage.setItem('board', JSON.stringify([pixelData]))
+      }
+
+      pixel.addEventListener('click', async ({ target }) => {
         const divColor = document.querySelector('.selected')
         let color;
 
+        const crud = localStorage.getItem('board')
+        const data = await JSON.parse(crud)
+        
         if (divColor.value) {
           color = divColor.value
         } else {
           color = window.getComputedStyle(divColor).getPropertyValue('background-color')
         }
+
+        const prevC = window.getComputedStyle(target).getPropertyValue('background-color');
+        const crudL = localStorage.getItem('last-pixel')
+        const dataL = await JSON.parse(crudL)
+        localStorage.setItem('last-pixel', JSON.stringify(
+          [
+            ...dataL,
+            data.find((item) => item.l === index && item.p === indexY),
+          ]
+        ))
+
+        localStorage.setItem('board', JSON.stringify(data.map((item) => {
+          if (item.l === index && item.p === indexY) {
+            item.c = prevC
+          }
+          
+          return item
+        })))
+
+        validateEvent(document.querySelector('.return'))
 
         target.style.backgroundColor = color
       })
@@ -63,6 +90,9 @@ createBtn.addEventListener('click', () => {
 
     board.appendChild(div)
   }
+
+  lines.value = ''
+  pixels.value = ''
 })
 
 removeBtn.addEventListener('click', () => {
@@ -71,3 +101,28 @@ removeBtn.addEventListener('click', () => {
     pixel.style.backgroundColor = '#ffffff'
   })
 })
+
+returnBtn.addEventListener('click', async ({ target }) => {
+  const pixels = document.querySelectorAll('.pixel')
+  const crud = localStorage.getItem('last-pixel')
+  const data = await JSON.parse(crud)
+  const x = data.length - 1
+
+  const pixel = [...pixels].find((pixel) => pixel.id === `${data[x].l}-${data[x].p}`)
+
+  pixel.style.backgroundColor = data[x].c
+
+  localStorage.setItem('last-pixel', JSON.stringify(data.slice(0, -1)))
+  validateEvent(target)
+})
+
+const validateEvent = async (target) => {
+  const crud = localStorage.getItem('last-pixel')
+  const data = await JSON.parse(crud)
+
+  if (data.length === 0) {
+    target.disabled = true
+  } else {
+    target.disabled = false
+  }
+}
